4233faa4641a6a255534ebea3458fcbf86b9e348
magley <magazinlazar2@gmail.com>
2024-12-09 16:53:09 +0100

    feat: Improve bonus UI, add colliders to rig

--- Assets/Game/Breakout/UIBonus.cs
+++ Assets/Game/Breakout/UIBonus.cs
@@ -1,16 +1,27 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UIElements.Image;
 
 public class UIBonus : MonoBehaviour
 {
-	private GameObject MyUIContainer;
 	[SerializeField] private Text UIComboText;
+	[SerializeField] private GameObject Background;
+	[SerializeField] private Text Title;
 	[SerializeField] private float KeepUIOnScreenTimer = -1;
+	private AudioSource sourceBonus;
+
+	private bool IsBeingShown = false;
+
+	private float uiComboTextSizeCurrent = 157;
+	private int uiComboTextSizeDesired = 157;
 
 	private void Start()
 	{
 		Ball.OnComboMade += ShowCombo;
-		MyUIContainer = transform.GetChild(0).gameObject;
+		UIComboText.text = "";
+		sourceBonus = GetComponent<AudioSource>();
 	}
 
 	private void Update()
@@ -25,6 +36,33 @@ public class UIBonus : MonoBehaviour
 				KeepUIOnScreenTimer = -1;
 			}
 		}
+
+		{
+			Color col = Background.GetComponent<RawImage>().color;
+			float targetAlpha = IsBeingShown ? 1f : 0f;
+			col.a = Mathf.Lerp(col.a, targetAlpha, 0.15f);
+			Background.GetComponent<RawImage>().color = col;
+		}
+		{
+			Color col = UIComboText.color;
+			float targetAlpha = IsBeingShown ? 1f : 0f;
+			col.a = Mathf.Lerp(col.a, targetAlpha, 0.15f);
+			UIComboText.color = col;
+		}
+		{
+			Color col = Title.color;
+			float targetAlpha = IsBeingShown ? 1f : 0f;
+			col.a = Mathf.Lerp(col.a, targetAlpha, 0.15f);
+			Title.color = col;
+		}
+		{
+			if (uiComboTextSizeCurrent > uiComboTextSizeDesired)
+			{
+				uiComboTextSizeCurrent -= 4;
+			}
+
+			UIComboText.fontSize = (int)uiComboTextSizeCurrent;
+		}
 	}
 
 	private void ShowCombo(int combo)
@@ -32,15 +70,21 @@ public class UIBonus : MonoBehaviour
 		ShowUI();
 		UIComboText.text = $"{combo}";
 		KeepUIOnScreenTimer = 3;
+
+		// Play combo ding
+		sourceBonus.pitch = 1 + ((Mathf.Max(combo, 3) - 3) / 7f);
+		sourceBonus.Play();
 	}
 
 	private void ShowUI()
 	{
-		MyUIContainer.SetActive(true);
+		IsBeingShown = true;
+		uiComboTextSizeCurrent = uiComboTextSizeDesired + 100;
+		UIComboText.GetComponent<RectTransform>().rotation = Quaternion.Euler(0f, 0f, Random.Range(-5f, 5f) * 2.5f);
 	}
 
 	private void HideUI()
 	{
-		MyUIContainer.SetActive(false);
+		IsBeingShown = false;
 	}
 }
