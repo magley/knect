324a3bc3666275bc643f83286501a8de8eb5de08
magley <60552482+magley@users.noreply.github.com>
2024-12-16 11:40:48 +0100

    feat: Slightly move camera with player collider

--- Assets/Game/Breakout/BreakoutCameraController.cs
+++ Assets/Game/Breakout/BreakoutCameraController.cs
@@ -1,18 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BreakoutCameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+    private List<Collider> playerColliders;
+    [SerializeField] private Vector3 myCentroid;
+    private Vector3 initialCentroid;
+    private Vector3 initialPosition;
+
     void Start()
     {
-        
-    }
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerColliders = player.transform.GetComponentsInChildren<Collider>().ToList();
 
-    // Update is called once per frame
+        myCentroid = GetPlayerCentroid();
+        initialCentroid = myCentroid;
+        initialPosition = transform.position;
+	}
+
     void Update()
     {
-        
-    }
+        Vector3 targetCentroid = GetPlayerCentroid();
+        myCentroid = Vector3.Lerp(myCentroid, targetCentroid, 0.25f);
+        myCentroid = initialCentroid + (myCentroid - initialCentroid) * 0.2f;
+
+		transform.LookAt(myCentroid, Vector3.up);
+        transform.position = initialPosition + new Vector3((myCentroid - initialCentroid).x, (myCentroid - initialCentroid).y, 0) * 0.9f;
+	}
+
+    private Vector3 GetPlayerCentroid()
+    {
+		Vector3 playerCentroid = Vector3.zero;
+		foreach (var cld in playerColliders)
+		{
+			playerCentroid += cld.transform.position;
+		}
+
+		playerCentroid /= playerColliders.Count;
+        return playerCentroid;
+	}
 }
