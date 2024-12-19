96402fa60a251b39fe1f8c358ecd7a44fdecbb21
magley <60552482+magley@users.noreply.github.com>
2024-12-19 12:30:52 +0100

    feat: Change `on time out` behavior

--- Assets/Game/Breakout/LevelManager.cs
+++ Assets/Game/Breakout/LevelManager.cs
@@ -14,13 +14,20 @@ public class LevelManager : MonoBehaviour
 	[SerializeField] private Text TimeLeftText;
 	private float secondsLeft = 60;
 	[SerializeField] private AudioSource DrumRollLoop;
+	[SerializeField] private AudioClip SndRefereeWhistle;
+	[SerializeField] private AudioClip SndDrumRoll;
 	[SerializeField] private AudioClip SndDrumRollEnd;
-
-	private bool ShouldAddPointsForRemainingTime = false;
 
 	[SerializeField] private List<GameObject> PrefabWavesInOrder = new List<GameObject>();
 	private int waveIndex = 0;
 	private int waveCounter = 0;
+	/// <summary>
+	/// When the game ends, the visible wave counter drops one by one and gives you points.
+	/// We want to keep track of the real counter (i.e. total number of waves) just in case.
+	/// </summary>
+	private int waveCounterVisible = 0;
+
+	private bool IsGameGoing = true;
 
 	void Start()
 	{
@@ -36,12 +43,18 @@ public class LevelManager : MonoBehaviour
 
 	public void SpawnNextWave()
 	{
+		if (!IsGameGoing)
+		{
+			return;
+		}
+
 		if (PrefabWavesInOrder.Count > 0)
 		{
 			var obj = PrefabWavesInOrder[waveIndex];
 			Instantiate(obj);
 			waveIndex = (waveIndex + 1) % (PrefabWavesInOrder.Count);
 			waveCounter++;
+			waveCounterVisible++;
 		}
 		else
 		{
@@ -61,59 +74,59 @@ public class LevelManager : MonoBehaviour
 		int minutes = (int)secondsLeft / 60;
 		int seconds = (int)secondsLeft % 60;
 		TimeLeftText.text = $"{minutes.ToString("D2")}:{seconds.ToString("D2")}";
-		TimeLeftText.text += $"\nWave {waveCounter}";
+		TimeLeftText.text += $"\nWave {waveCounterVisible}";
 
 		// Tick time.
 
 		if (ShouldTickTimeLeft)
 		{
 			secondsLeft -= Time.deltaTime;
-			if (secondsLeft <= 0)
+			if (secondsLeft <= 0 && IsGameGoing)
 			{
 				TimeIsUp();
-			}
-		}
-
-		if (ShouldAddPointsForRemainingTime)
-		{
-			if (secondsLeft > 0)
-			{
-				secondsLeft -= 1;
-				GameState.AddScore(100);
-
-				if (secondsLeft <= 0)
-				{
-					DrumRollLoop.Stop();
-					DrumRollLoop.clip = SndDrumRollEnd;
-					DrumRollLoop.loop = false;
-					DrumRollLoop.Play();
-
-					secondsLeft = 0;
-					Invoke(nameof(PromptForNextLevel), 0.75f);
-				}
 			}
 		}
 	}
 
 	private void TimeIsUp()
 	{
-
-	}
-
-	public void OnLevelComplete()
-	{
 		ShouldTickTimeLeft = false;
-		Invoke(nameof(DestoryRandomBall), 1f);
+		IsGameGoing = false;
+
+		DrumRollLoop.clip = SndRefereeWhistle;
+		DrumRollLoop.loop = false;
+		DrumRollLoop.Play();
+
+		Invoke(nameof(TimeIsUp_01_StopBalls), 0.25f);
 	}
 
-	private void DestoryRandomBall()
+	private void TimeIsUp_01_StopBalls()
+	{
+		foreach(var ball in FindObjectsByType<Ball>(FindObjectsSortMode.None))
+		{
+			ball.GetComponent<Rigidbody>().velocity *= 0;
+		}
+
+		Invoke(nameof(TimeIsUp_02_PlayDrumRoll), 1.473f);
+	}
+
+	private void TimeIsUp_02_PlayDrumRoll()
+	{
+		DrumRollLoop.clip = SndDrumRoll;
+		DrumRollLoop.loop = true;
+		DrumRollLoop.Play();
+
+		Invoke(nameof(TimeIsUp_03_DestroyRandomBall), 0.4f);
+	}
+
+	private void TimeIsUp_03_DestroyRandomBall()
 	{
 		var balls = FindObjectsByType<Ball>(FindObjectsSortMode.InstanceID).ToList();
 		balls.Sort((Ball b1, Ball b2) => { return b1.transform.position.z.CompareTo(b2.transform.position.z); });
 		balls.Reverse();
 		if (balls.Count == 0)
 		{
-			Invoke(nameof(OnAllBallsDestroyed), 1f);
+			Invoke(nameof(TimeIsUp_04_AllBallsDestroyed), 0.1f);
 		}
 		else
 		{
@@ -130,20 +143,41 @@ public class LevelManager : MonoBehaviour
 			Destroy(ball);
 
 			sndBallDestroyOnLevelEnd.Play();
-			sndBallDestroyOnLevelEnd.pitch += 0.15f;
+			sndBallDestroyOnLevelEnd.pitch += 0.1f;
 
-			Invoke(nameof(DestoryRandomBall), 0.25f);
+			Invoke(nameof(TimeIsUp_03_DestroyRandomBall), 0.25f);
 		}
 	}
 
-	private void OnAllBallsDestroyed()
+	private void TimeIsUp_04_AllBallsDestroyed()
 	{
-		ShouldAddPointsForRemainingTime = true;
-		DrumRollLoop.Play();
+		Invoke(nameof(TimeIsUp_05_GetPointsForClearedWave), 0.15f);
 	}
 
-	private void PromptForNextLevel()
+	private void TimeIsUp_05_GetPointsForClearedWave()
 	{
+		if (waveCounterVisible > 0)
+		{
+			GameState.AddScore(1000);
 
+			sndBallDestroyOnLevelEnd.Play();
+			sndBallDestroyOnLevelEnd.pitch += 0.1f;
+
+			waveCounterVisible--;
+
+			Invoke(nameof(TimeIsUp_05_GetPointsForClearedWave), 0.25f);
+		}
+		else
+		{
+			Invoke(nameof(TimeIsUp_06_ShowResultsAndMenu), 0.67f);
+		}
+	}
+
+	private void TimeIsUp_06_ShowResultsAndMenu()
+	{
+		DrumRollLoop.Stop();
+		DrumRollLoop.clip = SndDrumRollEnd;
+		DrumRollLoop.loop = false;
+		DrumRollLoop.Play();
 	}
 }
